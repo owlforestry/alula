b1c4776810a025ed9c8b933c6f5322576c299ebe
Mikko Kokkonen <mikko@mikian.com>
2012-04-22 21:08:48 +0300

    Skip empty javascript/stylesheet links. Inline small ones.

--- lib/alula/engine/helpers/assets.rb
+++ lib/alula/engine/helpers/assets.rb
@@ -3,12 +3,30 @@ module Alula
     module Helpers
       def stylesheet_link(name = "styles")
         name += ".css" if File.extname(name).empty?
-        "<link rel=\"stylesheet\" href=\"#{asset_url(name)}\" type=\"text/css\" />"
+        if asset_url(name)
+          # Inline?
+          if File.size(File.join("public", asset_url(name))) > 10
+            "<link rel=\"stylesheet\" href=\"#{asset_url(name)}\" type=\"text/css\" />"
+          else
+            content = File.read(File.join("public", asset_url(name)))
+            "<style type=\"text/css\">#{content}</style>"
+          end
+        end
       end
       
       def javascript_link(name = "scripts")
         name += ".js" if File.extname(name).empty?
-        "<script type=\"text/javascript\" src=\"#{asset_url(name)}\"></script>"
+        if asset_url(name)
+          # Inline?
+          if File.size(File.join("public", asset_url(name))) > 10
+            "<script type=\"text/javascript\" src=\"#{asset_url(name)}\"></script>" if asset_url(name)
+          else
+            content = File.read(File.join("public", asset_url(name)))
+            unless content == ";"
+              "<script type=\"text/javascript\">#{content}</script>"
+            end
+          end
+        end
       end
       
       def asset_url(name)
