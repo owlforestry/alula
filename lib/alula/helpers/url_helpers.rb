a4017d933ba324c678046b3922f62848cd7b62ab
Mikko Kokkonen <mikko@mikian.com>
2012-07-12 18:52:03 +0300

    Added url_for helper to fill correct hostname

--- lib/alula/helpers/url_helpers.rb
+++ lib/alula/helpers/url_helpers.rb
@@ -1,5 +1,9 @@
 module Alula
   module Helpers
+    def url_for(name)
+      File.join(self.site.config.url, name)
+    end
+    
     def link_to(content, url, attributes = {})
       tag = "<a"
       tag += " href=\"#{url}\""
