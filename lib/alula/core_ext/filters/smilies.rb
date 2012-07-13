d1236eb59bb088090ab5c63bf518a9257145389b
Mikko Kokkonen <mikko@mikian.com>
2012-07-13 11:59:03 +0300

    Added new smilies classes (no support for images/theme yet)

--- lib/alula/core_ext/filters/smilies.rb
+++ lib/alula/core_ext/filters/smilies.rb
@@ -1,9 +1,28 @@
 module Alula
   class Smilies < Filter
     MAP = {
-      smile: %w{:-) :) ^^ ^_^},
-      sad: %w{:-( :(},
-      laugh: %w{:-D :D},
+      angel:       %w{:angel: },
+      angry:       %w{:angry: },
+      cool:        %w{:cool: },
+      badguy:      %w{:badguy: },
+      crying:      %w{:crying: ;( ;-( },
+      exclamation: %w{:exclamation: },
+      idea:        %w{:idea: },
+      kiss:        %w{:kiss: :* :-* },
+      laugh:       %w{:laugh: :D :-D },
+      monkey:      %w{:monkey: },
+      plain:       %w{:plain: :| :-| },
+      question:    %w{:question: },
+      tongue:      %w{:tongue: },
+      sad:         %w{:sad: :( :-( },
+      grin:        %w{:grin: },
+      smile:       %w{:smile: :) :-) ^^ ^_^},
+      smirk:       %w{:smirk: },
+      point_right: %w{:point_right: },
+      surprise:    %w{:surprise: },
+      uncertain:   %w{:uncertain: },
+      wink:        %w{:wink: ;) ;-) },
+      worried:     %w{:worried: },
     }
     SMILIES = Hash[*MAP.collect{|cls, keys| keys.collect{|key| [key, "<span class=\"smilies-#{cls}\">#{key}</span>"]}}.flatten]
     SMILIES_RE = Regexp.new(SMILIES.keys.collect{|k| Regexp.escape(k)}.join("|"))
