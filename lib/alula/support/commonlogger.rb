97e844f2c081bbab1784396858bf072ddf1914ee
Mikko Kokkonen <mikko@mikian.com>
2012-07-15 12:53:47 +0300

    Added error code and timing to preview logging

--- lib/alula/support/commonlogger.rb
+++ lib/alula/support/commonlogger.rb
@@ -3,7 +3,7 @@ module Alula
     # Common Log Format: http://httpd.apache.org/docs/1.3/logs.html#common
     # lilith.local - - [07/Aug/2006 23:58:02] "GET / HTTP/1.1" 500 -
     #             %{%s - %s [%s] "%s %s%s %s" %d %s\n} %
-    FORMAT = %{[%s] %s %s\n}
+    # FORMAT = %{[%s] %s %s\n}
 
     def initialize(app, logger=nil)
       @app = app
@@ -11,8 +11,10 @@ module Alula
     end
 
     def call(env)
+      request_start = Time.now
       status, header, body = @app.call(env)
       header = Rack::Utils::HeaderHash.new(header)
+      env["REQUEST_TIME"] = (Time.now - request_start) * 1000.0
       body = Rack::BodyProxy.new(body) { log(env, status, header) }
       [status, header, body]
     end
@@ -21,9 +23,11 @@ module Alula
 
     def log(env, status, header)
       logger = @logger || env['rack.errors']
-      logger.write FORMAT % [
+      logger.write %{[%s] %s %d %.3fms %s\n} % [
         env["REMOTE_ADDR"],
         env["REQUEST_METHOD"],
+        status,
+        env["REQUEST_TIME"],
         Rack::Utils.unescape(env["PATH_INFO"])]
     end
   end
