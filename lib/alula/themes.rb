64fcfd2a9e131ec4a07f864f17bb39888f635ec5
Mikko Kokkonen <mikko@mikian.com>
2012-07-29 13:58:54 +0300

    Added support for theme versions

--- lib/alula/themes.rb
+++ lib/alula/themes.rb
@@ -1,5 +1,6 @@
 require 'alula/theme'
+require 'alula/themes/version'
 require 'haml'
 
 # Theme name, theme top-directory
-Alula::Theme.register :minimal, File.expand_path(File.join(File.dirname(__FILE__), %w{.. .. themes}))
+Alula::Theme.register :minimal, File.expand_path(File.join(File.dirname(__FILE__), %w{.. .. themes})), Alula::Themes::VERSION
