167d88fe3271b7c21f5409f625c60df7df686950
Mikko Kokkonen <mikko@mikian.com>
2012-08-11 16:55:47 +0300

    Preparing for 0.4.12 release

--- alula-themes/lib/alula/themes/version.rb
+++ alula-themes/lib/alula/themes/version.rb
@@ -3,7 +3,7 @@ module Alula
     module VERSION
       MAJOR = 0
       MINOR = 4
-      PATCH = 11
+      PATCH = 12
       PRE   = nil
 
       STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
