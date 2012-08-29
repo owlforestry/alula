ccff8f26ff5302696c4426e9674aaed358470978
Mikko Kokkonen <mikko@mikian.com>
2012-08-29 19:39:35 +0300

    Preparing for 0.4.21 release

--- alula-plugins/lib/alula/plugins/version.rb
+++ alula-plugins/lib/alula/plugins/version.rb
@@ -3,7 +3,7 @@ module Alula
     module VERSION
       MAJOR = 0
       MINOR = 4
-      PATCH = 20
+      PATCH = 21
       PRE   = nil
 
       STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
