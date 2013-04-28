107f820fd0e3b29c0d296dce84501693a451ff1d
Mikko Kokkonen <mikko@mikian.com>
2013-04-29 00:06:29 +0300

    Optimized RUM code
    Ran RUM javascript through UglifyJS and optimised one extra parenthesis away. Every byte counts…
    
    Signed-off-by: Mikko Kokkonen <mikko@mikian.com>

--- alula-plugins/lib/alula/plugins/pingdom.rb
+++ alula-plugins/lib/alula/plugins/pingdom.rb
@@ -16,7 +16,7 @@ module Alula
       
       # Register addons
       Alula::Plugin.addon :head, ->(context) {
-        "<script>var _prum=[['id','#{options.token}'],['mark','firstbyte',(new Date()).getTime()]];(function(){var s=document.getElementsByTagName('script')[0],p=document.createElement('script');p.async='async';p.src='//rum-static.pingdom.net/prum.min.js';s.parentNode.insertBefore(p,s);})();</script>"
+        "<script>var _prum=[['id','#{options.token}'],['mark','firstbyte',(new Date).getTime()]];(function(){var s=document.getElementsByTagName('script')[0],p=document.createElement('script');p.async='async';p.src='//rum-static.pingdom.net/prum.min.js';s.parentNode.insertBefore(p,s)})();</script>"
       }
     end
   end
